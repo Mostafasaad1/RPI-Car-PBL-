7b06a531005f9def1a2fae9f59e421af407e04fc
Abdelrahman Ahmed Omar Abdelgawad <93412453+AbdelrahmanAbdelgwad@users.noreply.github.com>
2022-06-30 00:33:19 +0200

    Delete First directory

--- First/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python3 manage.py runserver 169.254.120.58:8000  & ./stream/mjpg_streamer -i "stream/input_uvc.so -d /dev/video0 -r 640x480 -f 60" -o "stream/output_http.so -w stream/www" 
